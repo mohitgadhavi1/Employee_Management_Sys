ec745f6756c67215426e7d79081fddf178b697f9
Mohit Gadhavi <mohitgadhavi1@gmail.com>
2023-12-04 01:55:54 +0530

    client git fixed

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,10 +1,8 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-
-const verifyTokenManager= (req, res, next) => {
-
-  const token = req.headers.authorization.split(' ')[1];
+const verifyTokenManager = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1] || null;
 
   if (!token) {
     return res
@@ -18,48 +16,46 @@ const verifyTokenManager= (req, res, next) => {
     req.user = decoded;
 
     next();
-  }catch (error) {
+  } catch (error) {
     console.error(error.name);
 
- 
-    if (error.name === 'TokenExpiredError') {
-      return res.status(401).json({ error: 'Token has expired' });
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token has expired" });
     }
-    if (error.name === 'JsonWebTokenError') {
-        return res.status(401).json({ error: 'Invalid token' });
-      }
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ error: "Invalid token" });
+    }
 
-      res.status(401).json({ error: 'Authentication error' })
+    res.status(401).json({ error: "Authentication error" });
   }
 };
 const verifyTokenEmployee = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1] || null;
 
-    const token = req.headers.authorization.split(' ')[1];
-  
-    if (!token) {
-      return res
-        .status(401)
-        .json({ error: "Access denied. Token not provided." });
-    }
-  
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY_EMPLOYEE);
-      req.user = decoded;
-  
-      next();
-    }catch (error) {
-      console.error(error.name);
-  
-      // Check if the error is due to token expiration
-      if (error.name === 'TokenExpiredError') {
-        return res.status(401).json({ error: 'Token has expired' });
-      }
-      if (error.name === 'JsonWebTokenError') {
-          return res.status(401).json({ error: 'Invalid token' });
-        }
-  
-        res.status(401).json({ error: 'Authentication error' })
-    }
-  };
+  if (!token) {
+    return res
+      .status(401)
+      .json({ error: "Access denied. Token not provided." });
+  }
 
-module.exports = {verifyTokenManager,verifyTokenEmployee}
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY_EMPLOYEE);
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    console.error(error.name);
+
+    // Check if the error is due to token expiration
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ error: "Token has expired" });
+    }
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+
+    res.status(401).json({ error: "Authentication error" });
+  }
+};
+
+module.exports = { verifyTokenManager, verifyTokenEmployee };
